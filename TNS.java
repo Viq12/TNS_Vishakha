0d863c8ba7914edf1cc1cb3600ce44763cfb84c7
Vishakha Bhosale <130587812+Viq12@users.noreply.github.com>
2024-06-27 20:46:01 +0530

    Add files via upload

---
+++ TNS.java
@@ -0,0 +1,71 @@
+class TNS{
+    public static void main(String []args){
+        system.out.println("Hello World!");
+        private string name;
+        private int income;
+        private string gender;
+        private int age;
+        private int tax;
+        //private cann't be accessed publically to access it we have to use getter setter method. But can't change it's datatype
+    }
+}
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public void settax(int tax){
+        this.tax = tax;
+    }
+    public string Tostring(){
+        return "Person " + 
+    }
+    public class TaxCalculator{
+        public void calculateTax(Person person){
+            if(person.getAge() > 65 || person.getGender().equalsIgnoreCase("female")){
+                person.setTax(0);
+                system.out.println("Tax not applicable");
+            }
+            else{
+                if(person.getIncome() <= 160000){
+                    person.setTax(0);
+                }
+                else if (person.getIncome() > 160000 && person.getIncome() <= 500000){
+                    peerson.setTax((person.getIncome() - 160000) * 10/100);
+                }
+                else if (person.getIncome() > 500000 && person.getIncome() <= 800000){
+                    peerson.setTax((person.getIncome() - 500000) * 20/100 + 34000);
+                }
+                else {
+                    peerson.setTax((person.getIncome() - 800000) * 30/100 + 94000);
+                }
+        }
+        
+    }
+     //Scanner : 
+     public static void main(String{} args){
+         Scanner ob=new Scanner(System.in);
+         String name;
+         System.out.println("Enter Person Name :");
+         name =ob.next();
+         System.out.println("Enter age: ");
+         int age= ob.nextInt();
+         System.out.println("Enter Gender: ");
+         
+         System.out.println("Enter Income: ");
+         
+     }
+     
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        private string name;
+        private int income;
+        private string gender;
+        private int age;
+        private int tax;
+        
+    }
+    System.out.println(person)
+     
+    
+}
